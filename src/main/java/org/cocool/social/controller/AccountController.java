4d1fa8600d916da9922c5250cf305b1cf26c1253
tony <buaa>
2015-02-11 00:59:13 +0800

    注解

--- src/main/java/org/cocool/social/controller/AccountController.java
+++ src/main/java/org/cocool/social/controller/AccountController.java
@@ -1,12 +1,11 @@
 package org.cocool.social.controller;
 
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.cocool.social.controller.base.BaseController;
 import org.cocool.social.model.Account;
 import org.cocool.social.service.AccountService;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,14 +16,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AccountController extends BaseController{//怎么去访问这个Controller呢？
 	//这个/accountController就是地址：http:localhost:8080/cocool/accountController/showAccount.do
 	
-	protected AccountService accountService;
+	protected AccountService accountService;//业务处
 
 	public AccountService getAccountService() {
 		return accountService;
 	}
 
 	@Autowired
-	public void setAccountService(AccountService accountService) {
+	public void setAccountService(AccountService accountService) {//自动注入
 		this.accountService = accountService;
 	}
 	
@@ -32,16 +31,18 @@ public class AccountController extends BaseController{//怎么去访问这个Con
 	//id是b变量，@PathVariable该注解说明id是一个变量
 	public String showAccount(@PathVariable Integer id, HttpServletRequest request) {
 		Account a = accountService.getUserById(id);
-		//将user存到request当中，存完之后，返回到showUser.jsp中
-		request.setAttribute("account", a);//键值是accountName
+		//将account存到request当中，存完之后，返回到showUser.jsp中
+		request.setAttribute("account", a);//键值是account
 		System.out.println(a.getAname());
 		//@RequestMapping("/showAccount")
 		//访问地址 http:localhost:8080/cocool/accountController/showAccount.do
+		
 		//@RequestMapping("/showAccount/{id}")
 		//http:localhost:8080/cocool/accountController/showAccount/1.do
+		
 		//访问地址@RequestMapping("/{id}/showUser")
 		//http:localhost:8080/cocool/accountController/1/showAccount.do
-		return "showAccount";//showUser表示视图名称，或是一个.jsp页面的名称
+		return "showAccount";//showAccount表示视图名称，或是一个.jsp页面的名称
 	}
 	
 }
