7f7cbe071a83507c4734f8440d8a1eced923abfa
tony <buaa>
2015-02-10 12:59:35 +0800

    注解完善

--- src/main/java/org/cocool/social/service/impl/AccountServiceImpl.java
+++ src/main/java/org/cocool/social/service/impl/AccountServiceImpl.java
@@ -8,18 +8,21 @@ import org.cocool.social.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 //实现类
+//通过@Service注解，将该类放到Spring上下文环境中，告诉Spring，我这个类是一个Bean，并加入到Spring环境中
+//你这个userService在程序启动时，spring自动扫描时，就会自动添加到spring容器中（即，spring上下文中）
 @Service("accountService")
 public class AccountServiceImpl implements AccountService {
-	//通过@Service注解，将该类放到Spring上下文环境中，告诉Spring，我这个类是一个Bean，并加入到Spring环境中
 	
 	private AccountMapper accountMapper;
 	
-	public AccountMapper getUserMapper() {
+	public AccountMapper getAccountMapper() {
 		return accountMapper;
 	}
 
 	@Autowired
-	public void setUserMapper(AccountMapper accountMapper) {//通过@Autowired将userMapper注入到service中
+	public void setAccountMapper(AccountMapper accountMapper) {
+		//通过@Autowired将accountMapper注入到service中
+		//通过Spring将Service注入到Spring容器中（上下文中）
 		this.accountMapper = accountMapper;
 	}
 	
--- src/test/java/org/cocool/social/test/TestAccountService.java
+++ src/test/java/org/cocool/social/test/TestAccountService.java
@@ -5,22 +5,28 @@ import static org.junit.Assert.*;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.apache.log4j.Logger;
 import org.cocool.social.model.Account;
 import org.cocool.social.service.AccountService;
 
+
 import com.alibaba.fastjson.JSON;
 
+//结合了spring-test之后，则需要以下两处注解（如果不采用spring-test方式，则需要手动获取spring上下文）
 @RunWith(SpringJUnit4ClassRunner.class)
-//spring-test 第一步 加上@RunWith(SpringJUnit4ClassRunner.class)注解，这句话的意思是继承：extends SpringJUnit4ClassRunnser继承该类的注解
+/*
+ * spring-test 第一步 加上@RunWith(SpringJUnit4ClassRunner.class)注解，这句话的意思是继承：extends SpringJUnit4ClassRunnser继承该类的注解
+ * 等同于 public class TestMybatis extends SpringJUnit4ClassRunnser
+ */
 
 @ContextConfiguration(locations = { "classpath:spring.xml", "classpath:spring-mybatis.xml" })
-
 //告诉当前类你当前spring上下文需要加载那些配置文件！ @ContextConfiguration(locations = { "classpath:spring.xml", "classpath:spring-mybatis.xml" })
 public class TestAccountService {
 
@@ -30,6 +36,17 @@ public class TestAccountService {
 
 	private AccountService accountService;
 
+	/*
+	@Before
+	public void before()
+	{
+		//以前未用spring-test来测试时，采用这种方式来获取上下文，然后获取对应的service
+		//获取spring的上下文（spring初始化的时候很慢，又耗时也耗资源）
+		ApplicationContext ac = new ClassPathXmlApplicationContext(new String[]{ "spring.xml", "spring-mybatis.xml" });
+		accountService = (AccountService) ac.getBean("userService");//获取bean对象
+	}
+	*/
+	
 	public AccountService getAccountService() {
 		return accountService;
 	}
@@ -42,6 +59,13 @@ public class TestAccountService {
 	
 	@Test
 	public void testGetUserById() {
+		/*
+		//以前未用spring-test来测试时（未用spring的测试方法），采用这种方式来获取上下文，然后获取对应的service
+		//获取spring的上下文（spring初始化的时候很慢，又耗时也耗资源）
+		ApplicationContext ac = new ClassPathXmlApplicationContext(new String[]{ "spring.xml", "spring-mybatis.xml" });
+		AccountService accountService = （AccountService）ac.getBean("accountService");//获取bean对象
+		*/
+		
 		Account a = accountService.getUserById(1);
 		System.out.println(a.getAname());
 		//logger.info(JSON.toJSONStringWithDateFormat(a, "yyyy-MM-dd HH:mm:ss"));
